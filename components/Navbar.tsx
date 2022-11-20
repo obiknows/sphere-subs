1f7103a78f55d6e50c062dbe0dfbaf2ef71a35db
Obinna <obiknows88@gmail.com>
2022-11-20 19:25:45 +0200

    removed all dark directives

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,11 +3,11 @@ import Link from "next/link"
 export function Navbar() {
   return (
     <div className="h-12 w-full">
-      <div className="bg-white  rounded shadow-lg py-5 px-7">
+      <div className="bg-white  text-black rounded shadow-lg py-5 px-7">
         <nav className="flex justify-between">
           <div className="flex items-center space-x-3 lg:pr-16 pr-6">
             <Link href="/">
-              <h2 className="font-normal text-2xl leading-6 text-gray-800 dark:text-white">Sphere Subs 🌐</h2>
+              <h2 className="font-normal text-2xl leading-6 text-gray-800">Sphere Subs 🌐</h2>
             </Link>
           </div>
           <div className="flex space-x-5 justify-center items-center pl-2"></div>
