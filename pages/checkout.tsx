43f200db5374ef9968e2eb40135472bb1ec954af
Obinna <obiknows88@gmail.com>
2022-11-20 18:20:31 +0200

    added in boilerplate checkout to be loaded in popup window

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,97 +1,195 @@
-/* eslint-disable @next/next/no-img-element */
 import Head from "next/head"
-import Link from "next/link"
+import { useState } from "react"
 
-export default function Homepage() {
+export default function CheckOutPage() {
+  const countries = ["China", "Russia", "UK"]
+  const [menu, setMenu] = useState(false)
+  const [country, setCountry] = useState("United States")
+
+  // TODO: fix this any type, get specific
+  const changeText = (e: any) => {
+    setMenu(false)
+    setCountry(e.target.textContent)
+  }
+
   return (
     <>
       {/* Head */}
       <Head>
-        <title>Sphere Subs</title>
+        <title>Sphere Subs - Checkout</title>
       </Head>
       {/* Body */}
-      <div className=" overflow-hidden bg-white">
-        <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
-          <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
-            <div className="sm:max-w-lg">
-              <h1 className="font text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">The Subs are Here</h1>
-              <p className="mt-4 text-xl text-gray-500">
-                This year, we&apos;re changing the sandwich game for the better. Our new sandwich delivery subscription
-                will shelter you from the harsh elements of a world that doesn&apos;t care about subs
-              </p>
+      <div className="flex justify-center items-center">
+        <div className="py-16 px-4 md:px-6 2xl:px-0 flex justify-center items-center 2xl:mx-auto 2xl:container">
+          <div className="flex flex-col justify-start items-start w-full space-y-9">
+            <div className="flex justify-start flex-col items-start space-y-2">
+              <button className="flex flex-row items-center text-gray-600 hover:text-gray-500 space-x-1">
+                <svg
+                  className="fill-stroke"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 14 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M2.91681 7H11.0835"
+                    stroke="currentColor"
+                    strokeWidth="0.666667"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M2.91681 7L5.25014 9.33333"
+                    stroke="currentColor"
+                    strokeWidth="0.666667"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M2.91681 7.00002L5.25014 4.66669"
+                    stroke="currentColor"
+                    strokeWidth="0.666667"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+                <p className="text-sm leading-none">Back</p>
+              </button>
+              <p className="text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9 text-gray-800">Checkout</p>
             </div>
-            <div>
-              <div className="mt-10">
-                {/* Decorative image grid */}
-                <div
-                  aria-hidden="true"
-                  className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl">
-                  <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
-                    <div className="flex items-center space-x-6 lg:space-x-8">
-                      <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                        <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
-                          <img
-                            src="https://images.unsplash.com/photo-1485451456034-3f9391c6f769?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-                            alt=""
-                            className="h-full w-full object-cover object-center"
-                          />
-                        </div>
-                        <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img
-                            src="https://images.unsplash.com/photo-1619531038896-deaff53d151a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80"
-                            alt=""
-                            className="h-full w-full object-cover object-center"
-                          />
-                        </div>
-                      </div>
-                      <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                        <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img
-                            src="https://images.unsplash.com/photo-1639744093011-98a69e364923?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1046&q=80"
-                            alt=""
-                            className="h-full w-full object-cover object-center"
-                          />
-                        </div>
-                        <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img
-                            src="https://images.unsplash.com/photo-1629236055174-3e21cae6fa97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-                            alt=""
-                            className="h-full w-full object-cover object-center"
-                          />
-                        </div>
-                        <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img
-                            src="https://images.unsplash.com/photo-1642694358592-e4df77878e6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-                            alt=""
-                            className="h-full w-full object-cover object-center"
-                          />
-                        </div>
-                      </div>
-                      <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                        <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img
-                            src="https://images.unsplash.com/photo-1639667911189-700bd2029f5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-                            alt=""
-                            className="h-full w-full object-cover object-center"
-                          />
-                        </div>
-                        <div className="h-64 w-44 overflow-hidden rounded-lg">
-                          <img
-                            src="https://images.unsplash.com/photo-1639744093378-b2fde867b4d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-                            alt=""
-                            className="h-full w-full object-cover object-center"
-                          />
-                        </div>
-                      </div>
-                    </div>
+
+            <div className="flex flex-col xl:flex-row justify-center xl:justify-between space-y-6 xl:space-y-0 xl:space-x-6 w-full">
+              <div className="xl:w-3/5 flex flex-col sm:flex-row xl:flex-col justify-center items-center bg-gray-100 py-7 sm:py-0 xl:py-10 px-10 xl:w-full">
+                <div className="flex flex-col justify-start items-start w-full space-y-4">
+                  <p className="text-xl md:text-2xl leading-normal text-gray-800">Logitech K251</p>
+                  <p className="text-base font-semibold leading-none text-gray-600">$520.00</p>
+                </div>
+                <div className="mt-6 sm:mt-0 xl:my-10 xl:px-20 w-52 sm:w-96 xl:w-auto">
+                  <img src="https://i.ibb.co/0GFzTP4/Rectangle-131.png" alt="headphones" />
+                </div>
+              </div>
+
+              <div className="p-8 bg-gray-100 flex flex-col lg:w-full xl:w-3/5">
+                <button className="border border-transparent hover:border-gray-300 bg-gray-900 hover:bg-white text-white hover:text-gray-900 flex flex-row justify-center items-center space-x-2 py-4 rounded w-full">
+                  <div>
+                    <svg
+                      className="fill-current"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M10.9099 4.27692C9.6499 4.27692 9.1174 4.87817 8.2399 4.87817C7.34021 4.87817 6.65396 4.28129 5.56208 4.28129C4.49333 4.28129 3.35365 4.93379 2.6299 6.04535C1.61365 7.61285 1.78615 10.565 3.43208 13.08C4.02083 13.9804 4.80708 14.99 5.83833 15.001H5.85708C6.75333 15.001 7.01958 14.4141 8.25302 14.4072H8.27177C9.48677 14.4072 9.73052 14.9975 10.623 14.9975H10.6418C11.673 14.9866 12.5015 13.8679 13.0902 12.971C13.514 12.326 13.6715 12.0022 13.9965 11.2725C11.6155 10.3688 11.233 6.99348 13.5877 5.69942C12.869 4.79942 11.859 4.27817 10.9068 4.27817L10.9099 4.27692Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M10.6338 1C9.88379 1.05094 9.00879 1.52844 8.49629 2.15188C8.03129 2.71688 7.64879 3.555 7.79879 4.36781H7.85879C8.65754 4.36781 9.47504 3.88688 9.95254 3.27063C10.4125 2.68406 10.7613 1.85281 10.6338 1V1Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </div>
+                  <div>
+                    <p className="text-base leading-4">Pay</p>
+                  </div>
+                </button>
+
+                <div className="flex flex-row justify-center items-center mt-6">
+                  <hr className="border w-full" />
+                  <p className="flex flex-shrink-0 px-4 text-base leading-4 text-gray-600">or pay with card</p>
+                  <hr className="border w-full" />
+                </div>
+
+                <div className="mt-8">
+                  <input
+                    className="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600 text-gray-600"
+                    type="email"
+                    placeholder="Email"
+                  />
+                </div>
+
+                <label className="mt-8 text-base leading-4 text-gray-800">Card details</label>
+                <div className="mt-2 flex-col">
+                  <div>
+                    <input
+                      className="border rounded-tl rounded-tr border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
+                      type="email"
+                      placeholder="0000 1234 6549 15151"
+                    />
+                  </div>
+                  <div className="flex-row flex">
+                    <input
+                      className="border rounded-bl border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
+                      type="email"
+                      placeholder="MM/YY"
+                    />
+                    <input
+                      className="border rounded-br border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
+                      type="email"
+                      placeholder="CVC"
+                    />
                   </div>
                 </div>
 
-                <Link
-                  href="/store/product"
-                  className="inline-block rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-center font-medium text-white hover:bg-indigo-700">
-                  Shop The Subs
-                </Link>
+                <label className="mt-8 text-base leading-4 text-gray-800">Name on card</label>
+                <div className="mt-2 flex-col">
+                  <div>
+                    <input
+                      className="border rounded border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
+                      type="email"
+                      placeholder="Name on card"
+                    />
+                  </div>
+                </div>
+
+                <label className="mt-8 text-base leading-4 text-gray-800">Country or region</label>
+                <div className="mt-2 flex-col">
+                  <div className="relative">
+                    <button className="text-left border rounded-tr rounded-tl border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600 bg-white">
+                      {country}
+                    </button>
+                    <svg
+                      onClick={() => setMenu(!menu)}
+                      className={"transform  cursor-pointer absolute top-4 right-4 " + (menu ? "rotate-180" : "")}
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M3.5 5.75L8 10.25L12.5 5.75"
+                        stroke="#27272A"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                    <div
+                      className={
+                        "mt-1 absolute z-10 w-full flex bg-gray-50 justify-start flex-col text-gray-600 " +
+                        (menu ? "block" : "hidden")
+                      }>
+                      {countries.map((country) => (
+                        <div
+                          key={country}
+                          className="cursor-pointer hover:bg-gray-800 hover:text-white px-4 py-2"
+                          onClick={changeText}>
+                          {country}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                  <input
+                    className="border rounded-bl rounded-br border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
+                    type="text"
+                    placeholder="ZIP"
+                  />
+                </div>
+
+                <button className="mt-8 border border-transparent hover:border-gray-300 bg-gray-900 hover:bg-white text-white hover:text-gray-900 flex justify-center items-center py-4 rounded w-full">
+                  <div>
+                    <p className="text-base leading-4">Pay $54652</p>
+                  </div>
+                </button>
               </div>
             </div>
           </div>
