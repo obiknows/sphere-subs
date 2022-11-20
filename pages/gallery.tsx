4cbc5b169bf3034d96b4c09c1c4a48f728b3b992
Obinna <obiknows88@gmail.com>
2022-11-20 16:17:06 +0200

    added a boilerplate product page from the tailwind ui packaage

---
+++ pages/gallery.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head"
+
+export default function StoreGallery() {
+  return (
+    <div>
+      <Head>
+        <title>SPHERE SUBS STORE</title>
+      </Head>
+      <h1 className="text-3xl font-bold underline">SPHERE SUBS GALLERY 🌐🚇</h1>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        "[auto,auto,1fr]": "auto auto 1fr",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/aspect-ratio")],
 }
